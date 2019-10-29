6f4f2e7657247ec8962337a0dd37600c8a998966
Joris Hulsbosch <joris@portfolioris.nl>
2019-10-29 16:23:39 +0100

    add a slider

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,16 @@
 
 <script>
     import ModularPageTemplate from '../components/ModularPageTemplate.svelte';
+    import 'swiper/css/swiper.css';
+    import { Swiper } from 'swiper/js/swiper.esm.js';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        var mySwiper = new Swiper('.swiper-container', {
+          slidesPerView: 3,
+        });
+    });
+
     export let data;
     export let parsedContent;
 </script>
@@ -14,3 +24,12 @@
 <ModularPageTemplate modules={data.modules}></ModularPageTemplate>
 
 {@html parsedContent}
+
+<div class="swiper-container">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">Slide 1</div>
+        <div class="swiper-slide">Slide 2</div>
+        <div class="swiper-slide">Slide 3</div>
+        <div class="swiper-slide">Slide 4</div>
+    </div>
+</div>
