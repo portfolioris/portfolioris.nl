5f99babce7dd37c13cd02264b8c84be1fdbbb9b0
Joris Hulsbosch <joris@portfolioris.nl>
2019-10-29 16:30:09 +0100

    parse markdown in frontmatter

--- src/components/atoms/text/Text.svelte
+++ src/components/atoms/text/Text.svelte
@@ -1,8 +1,10 @@
 <script>
+    import marked from 'marked';
     export let text;
+    const ctext = marked(text);
 </script>
 
 <div class="u-module  u-m-b">
-    {@html text}
+    {@html ctext}
 </div>
 
