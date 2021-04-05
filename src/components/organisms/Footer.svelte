8026ec9162c035c8490e56b4407ed50091c45fc6
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2021-04-05 16:14:41 +0200

    add links to me pages

--- src/components/organisms/Footer.svelte
+++ src/components/organisms/Footer.svelte
@@ -1,6 +1,7 @@
 <script>
   import Layer from '../atoms/objects/Layer.svelte';
   import Retain from '../atoms/objects/Retain.svelte';
+  import Layout from '../atoms/objects/Layout.svelte';
 </script>
 
 <style type="text/scss" lang="scss">
@@ -10,12 +11,22 @@
     background-color: vars.$color-white;
     color: vars.$color-black;
   }
+
+  a {
+      #{vars.$global-interaction-states} {
+          color: var(--black);
+      }
+  }
 </style>
 
 <footer>
   <Layer size="medium">
     <Retain size="wall">
-      <p>© Portfolioris 2020</p>
+      <Layout fit gap="base">
+        <p>© Portfolioris 2021</p>
+        <p><a href="https://twitter.com/Portfolioris" rel="me">Find me on Twitter</a></p>
+        <p><a href="https://github.com/portfolioris" rel="me">Find me on GitHub</a></p>
+      </Layout>
     </Retain>
   </Layer>
 </footer>
