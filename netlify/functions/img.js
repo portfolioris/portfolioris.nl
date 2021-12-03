4b9e270dc5559c55afb794ef69a09ed083fa5446
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2021-12-03 22:03:25 +0100

    fix?

--- netlify/functions/img.js
+++ netlify/functions/img.js
@@ -1,5 +1,3 @@
-// const { builder } = require('@netlify/functions');
-// const fetch = require('node-fetch').default;
 import { builder } from '@netlify/functions';
 import fetch from 'node-fetch';
 
@@ -11,22 +9,8 @@ const API_ROOT = process.env.VITE_MEDIA_URL;
  */
 
 async function handler(event) {
-// exports.handler = async function (event) {
   const prefix = '/img/'; // need to be stripped of the path
-  // const prefix = '/.netlify/builders/img/'; // need to be stripped of the path
-  /*
-  const pieces = event.path.split('::');
-  let qs = '?quality=30&mode=crop';
-  // turn `width:300/height:300` into `?width=300&height=300
-  pieces[0].substring(prefix.length).split('/').forEach((param) => {
-    // param = width:300
-    const [prop, value] = param.split(':');
-    qs += `&${prop}=${value}`;
-  });
-  const url = `${API_ROOT}${pieces[1]}${qs}`;
-  */
   const url = `${API_ROOT}${event.path.substring(prefix.length)}`;
-  // console.log(event);
   try {
     const req = await fetch(url, {
       headers: {
