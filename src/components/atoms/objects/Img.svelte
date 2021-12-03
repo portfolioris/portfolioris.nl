6f107e8edab285d4ee078a94d80d704f33a27857
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2021-12-03 16:13:40 +0100

    fix?

--- src/components/atoms/objects/Img.svelte
+++ src/components/atoms/objects/Img.svelte
@@ -15,8 +15,8 @@
 </script>
 
 <img
-  src={`/img/q_auto,f_auto,c_scale,w_60/${cloudinaryUrl}`}
-  srcset={`${srcset}`}
-  alt={alt}
-  loading={lazyload ? 'lazy' : null}
+  src="{`/img/q_auto,f_auto,c_scale,w_60/${cloudinaryUrl}`}"
+  srcset="{`${srcset}`}"
+  alt="{alt}"
+  loading="{lazyload ? 'lazy' : null}"
 />
