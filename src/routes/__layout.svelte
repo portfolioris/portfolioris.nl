3dc15af97d940f03ef8fbe776512f7df6c12ecec
Joris Hulsbosch <joris.hulsbosch@weareyou.com>
2022-01-04 20:53:51 +0100

    fix build

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,12 @@
 <script context="module">
-  export async function load({ fetch, url }) {
+  export async function load({ fetch }) {
     const res = await fetch('/nav.json');
     const navItems = await res.json();
 
     return {
       props: {
         navItems,
-        url,
+        // url,
       },
     };
   }
@@ -14,13 +14,14 @@
 <script>
   import 'lazysizes';
   import 'wicg-inert';
-  // import { page } from '$app/stores';
+  import { page } from '$app/stores';
   import Navigation from '../components/organisms/Navigation.svelte';
 
   export let navItems = [];
-  export let url;
-  // console.log(page);
-  $: segment = url.pathname.split('/')[1];
+  // export let url;
+  console.log($page);
+  // let segment = '';
+  $: segment = $page.url.pathname.split('/')[1];
 </script>
 
 <style global lang="scss">
